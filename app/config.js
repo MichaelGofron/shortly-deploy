95471b5eaaeb5eda163f75b54e5f656ffe80dc87
Hack Reactor Students <accounts@hackreactor.com>
2015-07-30 15:48:05 -0700

    added azure mongoDB connection

--- app/config.js
+++ app/config.js
@@ -1,7 +1,7 @@
 var Bookshelf = require('bookshelf');
 var path = require('path');
 var mongoose = require('mongoose');
-var db = mongoose.createConnection('localhost', 'mike');
+var db = process.env.NODE_ENV === 'production' ? mongose.createConnection('mongodb://shortlyAdnanDB:pzqQzZg0h34bBSRCHfmnHx399yrjR0nv2tkZJraT9A4-@ds036698.mongolab.com:36698/shortlyAdnanDB') : mongoose.createConnection('localhost', 'mike');
 
 
 db.on('error', console.error.bind(console, 'connection error: '));
